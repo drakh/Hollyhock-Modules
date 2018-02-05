7d42cb643ff5016340aebe26b0338e61a26f0722
Lubomir Panak <lubopanak@gmail.com>
2018-02-05 10:12:57 +0100

    Euclidean Generator module for Hollyhock

--- EuclideanGenerator/EuclideanGenerator.cpp
+++ EuclideanGenerator/EuclideanGenerator.cpp
@@ -1,84 +1,76 @@
 #include "EuclideanGenerator.h"
-
-
-void CreateModule (void* &pModule, AnsiCharPtr optionalString, LongBool Flag, MasterInfo* pMasterInfo, AnsiCharPtr optionalContent)
-{
-	pModule = new EuclideanGenerator();
-}
-
-void DestroyModule (void* pModule) 
-{
-    // cast is important to call the good destructor
-	delete ((EuclideanGenerator*)pModule);
-}
+#include "bjorklund.h"
 
 const AnsiCharPtr UserModuleBase::MODULE_NAME = "euclidean generator";
 const AnsiCharPtr UserModuleBase::MODULE_DESC = "euclidean generator module";
 const AnsiCharPtr UserModuleBase::MODULE_VERSION = "1.0";
+const int MAX_SIZE = 256;
+int steps = 16;
+int beats = 0;
 
-void GetBrowserInfo(ModuleInfo* pModuleInfo) 
+void CreateModule(void* &pModule, AnsiCharPtr optionalString, LongBool Flag, MasterInfo* pMasterInfo, AnsiCharPtr optionalContent)
 {
-	pModuleInfo->Name				= UserModuleBase::MODULE_NAME;
-	pModuleInfo->Description		= UserModuleBase::MODULE_DESC;
-	pModuleInfo->Version			= UserModuleBase::MODULE_VERSION;
+	pModule = new EuclideanGenerator();
 }
 
-void EuclideanGenerator::onGetModuleInfo (MasterInfo* pMasterInfo, ModuleInfo* pModuleInfo) 
+void DestroyModule(void* pModule)
+{
+	delete ((EuclideanGenerator*)pModule);
+}
+
+void GetBrowserInfo(ModuleInfo* pModuleInfo)
+{
+	pModuleInfo->Name = UserModuleBase::MODULE_NAME;
+	pModuleInfo->Description = UserModuleBase::MODULE_DESC;
+	pModuleInfo->Version = UserModuleBase::MODULE_VERSION;
+}
+
+void EuclideanGenerator::onGetModuleInfo(MasterInfo* pMasterInfo, ModuleInfo* pModuleInfo)
 {
 
 	//identification of the module
-	pModuleInfo->Name				= MODULE_NAME;
-	pModuleInfo->Description		= MODULE_DESC;
-	pModuleInfo->ModuleType         = mtSimple;
-	pModuleInfo->BackColor          = sdkGetUsineColor(clDataModuleColor);
-	pModuleInfo->NumberOfParams     = 4;
-	pModuleInfo->Version			= MODULE_VERSION;
-	pModuleInfo->DontProcess		= TRUE;
+	pModuleInfo->Name = MODULE_NAME;
+	pModuleInfo->Description = MODULE_DESC;
+	pModuleInfo->ModuleType = mtSimple;
+	pModuleInfo->BackColor = sdkGetUsineColor(clDataModuleColor);
+	pModuleInfo->NumberOfParams = 4;
+	pModuleInfo->Version = MODULE_VERSION;
+	pModuleInfo->DontProcess = TRUE;
 	pModuleInfo->CanBeSavedInPreset = FALSE;
 
 }
 
-//-----------------------------------------------------------------------------
-// query system and init methodes
-//-----------------------------------------------------------------------------
-// query system not used
-
-//void DataMultiplyExampleExample::onInitModule (MasterInfo* pMasterInfo, ModuleInfo* pModuleInfo) 
-//{
-//}
-
-//----------------------------------------------------------------------------
-// parameters and process
-//----------------------------------------------------------------------------
-
-//-----------------------------------------------------------------------------
-// Parameters description
-void EuclideanGenerator::onGetParamInfo (int ParamIndex, TParamInfo* pParamInfo)
+void EuclideanGenerator::onGetParamInfo(int ParamIndex, TParamInfo* pParamInfo)
 {
 	// all parameters declared in the module class		
-	switch (ParamIndex) 
+	switch (ParamIndex)
 	{
-	// dtfInputA
+		// dtfInputA
 	case 0:
-		pParamInfo->ParamType		= ptDataField;
-		pParamInfo->Caption			= "length";
-		pParamInfo->IsInput			= TRUE;
-		pParamInfo->IsOutput		= FALSE;
-		pParamInfo->CallBackType	= ctImmediate;
-		pParamInfo->EventPtr        = &dtfInputLength;
-
+		pParamInfo->ParamType = ptDataField;
+		pParamInfo->Caption = "length";
+		pParamInfo->DefaultValue = (TPrecision)steps;
+		pParamInfo->IsInput = TRUE;
+		pParamInfo->IsOutput = FALSE;
+		pParamInfo->CallBackType = ctImmediate;
+		pParamInfo->EventPtr = &dtfInputLength;
+		pParamInfo->MaxValue = MAX_SIZE;
+		pParamInfo->MinValue = 0;
 		break;
 
-	// dtfInputB
+		// dtfInputB
 	case 1:
-		pParamInfo->ParamType		= ptDataField;
-		pParamInfo->Caption			= "pulses";
-		pParamInfo->IsInput			= TRUE;
-		pParamInfo->IsOutput		= FALSE;
-		pParamInfo->CallBackType	= ctImmediate;
-		pParamInfo->EventPtr        = &dtfInputPulses;
+		pParamInfo->ParamType = ptDataField;
+		pParamInfo->Caption = "pulses";
+		pParamInfo->DefaultValue = (TPrecision)beats;
+		pParamInfo->IsInput = TRUE;
+		pParamInfo->IsOutput = FALSE;
+		pParamInfo->CallBackType = ctImmediate;
+		pParamInfo->EventPtr = &dtfInputPulses;
+		pParamInfo->MaxValue = MAX_SIZE;
+		pParamInfo->MinValue = 0;
 		break;
-	// generate
+		// generate
 	case 2:
 		pParamInfo->ParamType = ptButton;
 		pParamInfo->Caption = "generate";
@@ -88,38 +80,47 @@ void EuclideanGenerator::onGetParamInfo (int ParamIndex, TParamInfo* pParamInfo)
 		pParamInfo->EventPtr = &dtfInputGenerate;
 		break;
 
-	// dftOutput
+		// dftOutput
 	case 3:
-		pParamInfo->ParamType		= ptArray;
-		pParamInfo->Caption			= "output";
-		pParamInfo->IsInput			= FALSE;
-		pParamInfo->IsOutput		= TRUE;
-		pParamInfo->DontSave		= TRUE;
-		pParamInfo->EventPtr        = &dftOutput;
-
+		pParamInfo->ParamType = ptArray;
+		pParamInfo->Caption = "output";
+		pParamInfo->IsInput = FALSE;
+		pParamInfo->IsOutput = TRUE;
+		pParamInfo->DontSave = TRUE;
+		pParamInfo->MinValue = 0;
+		pParamInfo->MaxValue = 1;
+		pParamInfo->ReadOnly = TRUE;
+		pParamInfo->IsShared = TRUE;
+		pParamInfo->CallBackType = ctImmediate;
+		pParamInfo->EventPtr = &dftOutput;
 		break;
 
-	// default case
+		// default case
 	default:
 		break;
 	}
 }
 
-//-----------------------------------------------------------------------------
-// Parameters callback
-void EuclideanGenerator::onCallBack (UsineMessage* Message)
+void EuclideanGenerator::onCallBack(UsineMessage* Message)
 {
 	try
 	{
-        if (Message->message == NOTIFY_MSG_USINE_CALLBACK)
-        {
-            int paramIndex = (int)Message->wParam;
-
-		    if ((paramIndex == 3) && (Message->lParam == MSG_CHANGE))
-		    {
-			    // sdkMultEvt3 (dtfInputA, dtfInputB, dftOutput);
-		    }
-        }
+		if (Message->message == NOTIFY_MSG_USINE_CALLBACK)
+		{
+			int paramIndex = (int)Message->wParam;
+			if ((paramIndex == 0) && (Message->lParam == MSG_CHANGE))
+			{
+				steps = (int)sdkGetEvtData(dtfInputLength);
+			}
+			if ((paramIndex == 1) && (Message->lParam == MSG_CHANGE))
+			{
+				beats = (int)sdkGetEvtData(dtfInputPulses);
+			}
+			if ((paramIndex == 2) && (Message->lParam == MSG_CHANGE))
+			{
+				updateCellsValues();
+			}
+		}
 	}
 	catch (...)
 	{
@@ -127,5 +128,19 @@ void EuclideanGenerator::onCallBack (UsineMessage* Message)
 	}
 }
 
+void EuclideanGenerator::onInitModule(MasterInfo* pMasterInfo, ModuleInfo* pModuleInfo) {
+	updateCellsValues();
+}
+
+void EuclideanGenerator::updateCellsValues()
+{
+	const std::string beat= bjorklund(beats, steps);
+	const char *cstr = beat.c_str();
+	sdkSetEvtSize(dftOutput, steps);
+	for (int i = 0; i < steps; i++)
+	{
+		sdkSetEvtArrayData(dftOutput, i, (TPrecision)(cstr[i] - '0'));
+	}
+}
 
 
---
+++ EuclideanGenerator/bjorklund.h
@@ -0,0 +1,6 @@
+// from https://bitbucket.org/sjcastroe/bjorklunds-algorithm
+#ifndef BJORKLUND_H
+#define BJORKLUND_H
+#include <string>
+std::string bjorklund(int beats, int steps);
+#endif
